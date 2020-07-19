e062907d4e1214499c00421cc67ba6695fadf7ab
Bladymir Tellez <btellez@gmail.com>
2020-07-19 18:57:58 -0500

    Update Either with an actual example

--- data/src/com/scaladocs/examples/either/package.scala
+++ data/src/com/scaladocs/examples/either/package.scala
@@ -6,17 +6,43 @@ import cats.implicits._
 package object either {
   def getPage: Page = new Page {
     val title = "Either"
+
     def signature= FQSignature("scala.util.Either[+A, +B]")
-    def description: Option[String] = None
-    def tags: List[Tag] = List.empty
-    def links: List[Link] = List.empty
+
+    def description: Option[String] = """
+    | Represents one of two possible values. Sometimes referred to as a
+    | disjoint union. An instance of Either will be one of: `scala.util.Left`
+    | or `scala.util.Right`.
+    """.stripMargin.trim.some
+
+    def tags: List[Tag] = List(
+      Tag("Standard Library")
+    )
+
+    def links: List[Link] = List(
+      Link("Docs", "https://www.scala-lang.org/api/2.13.1/scala/util/Either.html")
+    )
+
     def examples: CodeExamples = List(
       CodeExample(
-        description = Some("Basic Usage"), 
+        description = Some("Basic Usage: Representing Success or Failure"), 
         tags = List(Tag("Abc", "http://".some)),
         snippet = Code("""
-          |println("Hello World")
-          |println("Hello World...Again")
+          |import scala.util.{Either, Right, Left}
+          |
+          |object DiceGame {
+          |  private winRate = 0.5
+          |  private prizeAmount = 100
+          |  private rng = new Random()
+          |
+          |  def roll: Either[String, Long] = {
+          |    if (rng.nextFloat() < winRate) {
+          |      Right(100) // Winner 
+          |    } else {
+          |      Left("Sorry. Better Luck Next Time") // Loser
+          |    }
+          |  }
+          |}
           """.stripMargin.trim
         )
       )
