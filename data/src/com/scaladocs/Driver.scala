1375872330d75d2c6a6780895e1e73d93c7d9ea0
Bladymir Tellez <btellez@gmail.com>
2020-07-20 14:50:55 -0500

    Update Driver to Write HTML Output

--- data/src/com/scaladocs/Driver.scala
+++ data/src/com/scaladocs/Driver.scala
@@ -3,8 +3,46 @@ package com.scaladocs
 import cats._
 import cats.implicits._
 
+import com.scaladocs.io.Logger
+import java.nio.file.Paths
+import java.nio.file.Path
+import java.nio.file.Files
+
 object Driver extends App {
-  val eitherPage = examples.either.getPage
-  // TODO: Add Steps to Load Template Partials
-  println(eitherPage.show)
+  val root = Paths.get("").toAbsolutePath
+
+  // Add examples to this list to generate pages.
+  val exports = List(
+    examples.either.getPage
+  )
+
+  // Copy files required by the templates:
+  locally[Unit] {
+    val destinationClientPath = root.resolve("dest/")
+    val htmlRescourcesPath = root.resolve("data/resources/html/")
+
+    Files
+      .walk(htmlRescourcesPath)
+      .filter({ f => 
+        val notHTML = !f.getFileName().toString.endsWith(".html") 
+        val notRootDirectory = f.compareTo(htmlRescourcesPath) != 0
+        notHTML && notRootDirectory
+      })
+      .forEach(source => {
+        val fileName = source.getFileName
+        val destination = destinationClientPath.resolve(fileName)
+        io.copy(source, destination)
+      })
+  }
+
+  // Export the standalone Examples:
+  locally[Unit] { 
+    import html.StandaloneExamples._
+    Logger.info(s"Starting export for all pages. Exporting ${exports.size} Pages")
+    val outputPathPrefix = root.resolve("dest/examples/")
+    exports.foreach { page => 
+      val pageDestination = outputPathPrefix.resolve(page.canonicalPath)
+      io.putContents(pageDestination, page.show)
+    }
+  }
 }
